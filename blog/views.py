e0d9fa29a7165f72ee05932d04658cc1647d5179
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-29 17:25:21 +0300

    Blog Post Template

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,26 @@ posts = [
                         'title': 'Blog Post 2',
                         'content': 'Second post content',
                         'date_posted': 'August 28, 2021'
+                },
+                {
+                        'author': 'Lilian Muthoni',
+                        'title': 'Blog Post 3',
+                        'content': 'Third post content',
+                        'date_posted': 'August 29, 2021'
+                },
+                {
+                        'author': 'John Doe',
+                        'title': 'Blog Post 4',
+                        'content': 'Fourth post content',
+                        'date_posted': 'August 30, 2021'
                 }
 ]
 
 def home(request):
-        return render(request, 'blog/home.html')
+        context = {
+                'posts': posts
+        }
+        return render(request, 'blog/home.html', context)
 
 def about(request):
         return render(request, 'blog/about.html')
