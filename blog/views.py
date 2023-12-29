2b4d7d8d1acb6cf0467ebc99cbdad64c88571d2c
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-29 17:19:24 +0300

    Added posts lists

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+posts = [
+                {
+                        'author': 'Alex Mwaniki',
+                        'title': 'Full Stack Developer Roadmap',
+                        'content': 'First post content',
+                        'date_posted': 'August 27, 2021'
+                },
+                {
+                        'author': 'Jane Doe',
+                        'title': 'Blog Post 2',
+                        'content': 'Second post content',
+                        'date_posted': 'August 28, 2021'
+                }
+]
+
 def home(request):
         return render(request, 'blog/home.html')
 
