df14ff4add14253432ddf0d8134736ff0e88935a
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-31 00:30:36 +0300

    Update base.html with static file and navbar

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 posts = [
                 {
-                        'author': 'Alex Mwaniki',
-                        'title': 'Full Stack Developer Roadmap',
+                        'author': 'CoreyMS',
+                        'title': 'Blog Post 1',
                         'content': 'First post content',
-                        'date_posted': 'August 27, 2021'
+                        'date_posted': 'August 27, 2018'
                 },
                 {
                         'author': 'Jane Doe',
                         'title': 'Blog Post 2',
                         'content': 'Second post content',
-                        'date_posted': 'August 28, 2021'
+                        'date_posted': 'August 28, 2018'
                 }
 ]
 
