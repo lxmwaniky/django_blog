d32767a2e7e705c4443d569db13fc6742550e82c
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-03 00:24:26 +0300

    Add redirect after successful user registration

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -8,6 +8,7 @@ def register(request):
                 if form.is_valid():
                         username = form.cleaned_data.get('username')
                         messages.success(request, f'Account created for {username}!')
+                        return redirect('blog-home')
         else:
                 form = UserCreationForm()
         return render(request, 'users/register.html', {'form': form})
