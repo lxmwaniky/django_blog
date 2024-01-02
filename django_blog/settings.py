cc87ee8c9b391c9894ded5a34acae13c32b72f94
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-02 23:30:24 +0300

    Add 'user' app to INSTALLED_APPS

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'user.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
