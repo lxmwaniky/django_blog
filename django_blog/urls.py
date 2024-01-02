82b08fc44bd81bdd73ec98d91b017943d1236d35
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-02 23:59:02 +0300

    Fix import error and update app name in settings.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
+from users.views import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    path('register/', register, name='register'),
     path('', include('blog.urls')),
 ]
