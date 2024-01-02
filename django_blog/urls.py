38de73ccba2bace883a878edfd551dc402cf6da9
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-03 00:04:28 +0300

    Update register view and template path

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.views import views as user_views
-from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register, name='register'),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
         form = UserCreationForm()
-        return render(request, 'users/register.html', {'form': form})
+        return render(request, 'user/register.html', {'form': form})
