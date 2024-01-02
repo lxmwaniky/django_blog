f8bb2132f7a16c0b94c7dcf8c7a455f2095fdde1
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-03 00:23:48 +0300

    Add success message after user registration

--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
         if request.method == 'POST':
                 form = UserCreationForm(request.POST)
                 if form.is_valid():
                         username = form.cleaned_data.get('username')
+                        messages.success(request, f'Account created for {username}!')
         else:
                 form = UserCreationForm()
         return render(request, 'users/register.html', {'form': form})
