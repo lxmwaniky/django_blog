49f679ff3b2c839ac9b5178acd9aa0e8924b8ae8
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-01-03 00:01:49 +0300

    Add register view to urls.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.views import views as user_views
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
